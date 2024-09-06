2fc8ef4bee5cbf89768d5e38666d448614187c04
sepri7 <sepriadey@gmail.com>
2024-09-06 10:54:09 +0700

    done

--- 5. session/app.py
+++ 5. session/app.py
@@ -1,9 +1,16 @@
 from flask import*
+import secrets
+
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
 
 userAdmin = {
     "username" : "admin",
     "password" : "1234"
+}
+userAnggota = {
+    "username" : "anggota",
+    "password" : "4321"
 }
 
 @app.route('/')
@@ -13,20 +20,33 @@ def home():
 
 @app.route('/admin')
 def admin():
-    render_template("admin.html")
+    if session.get("user"):
+        return render_template("admin.html")
+    else:
+        return redirect(url_for("home"))
+        
 
 @app.route('/anggota')
 def anggota():
-    render_template("anggota.html")
+    return render_template("anggota.html")
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     username = request.form['username']
     password = request.form['password']
     if username == userAdmin["username"] and password == userAdmin["password"]:
+        session["user"] = username
         return redirect(url_for('admin'))
+    elif username == userAnggota["username"] and password == userAnggota["password"]:
+        session["user"] = username
+        return redirect(url_for('anggota'))
     else:
         return f"salah...."
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
