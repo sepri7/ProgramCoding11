aa5be495c455f5a7c55319867481350dd23aa374
sepri7 <sepriadey@gmail.com>
2024-11-01 11:30:28 +0700

    done

--- 6. CRUD flask-mysql (mysql.connector)/app.py
+++ 6. CRUD flask-mysql (mysql.connector)/app.py
@@ -3,15 +3,11 @@ import mysql.connector
 
 app = Flask(__name__)
 
-# Connect to server
-cnx = mysql.connector.connect(
+mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     database="sekolah",
     password="")
-
-# Get a cursor
-cur = cnx.cursor()
 
 @app.route('/')
 @app.route('/home')
@@ -20,52 +16,57 @@ def home():
 
 @app.route('/simpan', methods = ["POST", "GET"] )
 def simpan():
+
+    cursor = mydb.cursor()
     nama = request.form["nama"]
     kelas = request.form["kelas"]
     alamat = request.form["alamat"]
     query = ("insert into siswa values( %s, %s, %s, %s)")
     data = ( "", nama, kelas, alamat )
-    cur.execute( query, data )
-    cnx.commit()
-    cur.close()
-    return f"sukses disimpan.."
+    cursor.execute( query, data )
+    mydb.commit()
+    cursor.close()
+    return redirect("/tampil")
 
 @app.route('/tampil')
 def tampil():
-    cur.execute("select * from siswa")
-    data = cur.fetchall()
-    cur.close()
+    cursor = mydb.cursor()
+    cursor.execute("select * from siswa")
+    data = cursor.fetchall()
     return render_template('tampil.html',data=data) 
 
 @app.route('/hapus/<id>')
 def hapus(id):
+    cursor = mydb.cursor()
     query = ("delete from siswa where id = %s")
     data = (id,)
-    cur.execute( query, data )
-    mysql.connection.commit()
-    cur.close()
+    cursor.execute( query, data )
+    mydb.commit()
+    cursor.close()
     return redirect('/tampil')
 
 @app.route('/update/<id>')
 def update(id):
-    sql = ("select * from siswa where id = %s")
+    cursor = mydb.cursor()
+    query = ("select * from siswa where id = %s")
     data = (id,)
-    cur.execute( sql, data )
-    value = cur.fetchone()
+    cursor.execute( query, data )
+    value = cursor.fetchone()
     return render_template('update.html',value=value) 
 
 
 @app.route('/aksiupdate', methods = ["POST", "GET"] )
 def aksiupdate():
+    cursor = mydb.cursor()
     id = request.form["id"]
     nama = request.form["nama"]
     kelas = request.form["kelas"]
     alamat = request.form["alamat"]
     query = ("update siswa set nama = %s, kelas = %s, alamat = %s where id = %s")
     data = ( nama, kelas, alamat,id, )
-    cur.execute( query, data )
-    mysql.connection.commit()
-    cur.close()
+    cursor.execute( query, data )
+    mydb.commit()
+    cursor.close()
     return redirect('/tampil')
 
 if __name__ == "__main__":
