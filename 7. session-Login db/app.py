8e97427f906c85a6f184bc187dbef0d8c6995c46
sepri7 <sepriadey@gmail.com>
2024-11-14 09:42:17 +0700

    done

---
+++ 7. session-Login db/app.py
@@ -0,0 +1,53 @@
+from flask import*
+import secrets
+import mysql.connector
+
+app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    database="projectx",
+    password="")
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/aksi_login', methods =["POST", "GET"])
+def aksi_login():
+    cursor = mydb.cursor()
+    query = ("select * from user where username = %s and password = %s")
+    data = (request.form['username'], request.form['password'],)
+    cursor.execute( query, data )
+    value = cursor.fetchone()
+
+    username = request.form['username']
+    if value:
+        session["user"] = username
+        return redirect(url_for('admin'))
+    else:
+        return f"salah !!!"
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("home"))
+
+@app.route('/admin')
+def admin():
+    if session.get("user"):
+        return render_template("admin.html")
+    else:
+        return redirect(url_for("login"))
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
