bb79d48ad117eacb5c4e46956e70a432c0831d0a
sepri7 <sepriadey@gmail.com>
2024-11-18 13:44:00 +0700

    done

---
+++ 8. TMP_bootstrap5/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, url_for, redirect, session
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+if __name__ == "__main__": 
+    app.run(debug=True)
